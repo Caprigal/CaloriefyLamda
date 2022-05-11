65a0d78cc18718ba247da1897f86d98fa03739ef
Gál Péter <galpe23@gmail.com>
2022-05-11 23:48:41 +0200

    First commit

---
+++ functions/getUsers.js
@@ -0,0 +1,19 @@
+var pg = require('pg');
+//or native libpq bindings
+//var pg = require('pg').native
+
+var conString = process.env.PSQL_CONN_STRING //Can be found in the Details page
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT * FROM activities ORDER BY id ASC', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+});
