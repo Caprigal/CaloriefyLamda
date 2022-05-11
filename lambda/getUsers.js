3284a78abe48c129fb9877e15161254dd6c916ae
Gál Péter <galpe23@gmail.com>
2022-05-12 00:01:05 +0200

    Add handlers

--- lambda/getUsers.js
+++ lambda/getUsers.js
@@ -1,19 +1,30 @@
-var pg = require('pg');
+var pg = require('pg')
 //or native libpq bindings
 //var pg = require('pg').native
 
 var conString = process.env.PSQL_CONN_STRING //Can be found in the Details page
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT * FROM activities ORDER BY id ASC', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
+var client = new pg.Client(conString)
+
+exports.handler = (event, context, callback) => {
+  client.connect(function (err) {
+    if (err) {
+      return callback(null, {
+        statusCode: 400,
+        body: JSON.stringify(err),
+      })
     }
-    console.log(result.rows);
-    // >> output: 2018-08-23T14:02:57.117Z
-    client.end();
-  });
-});
+    client.query('SELECT * FROM activities ORDER BY id ASC', function (err, result) {
+      if (err) {
+        return callback(null, {
+          statusCode: 400,
+          body: JSON.stringify(err),
+        })
+      }
+      console.log(result.rows)
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(result.rows),
+      })
+    })
+  })
+}
