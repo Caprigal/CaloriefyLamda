5a32c4204dea047066d637f719c4c01c8a084633
Gál Péter <galpe23@gmail.com>
2022-05-27 02:46:32 +0200

    aa2

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,11 @@ module.exports = {
   devtool: "source-map",
   target: "node",
   alias: {
-    'pg-native': path.join(__dirname, 'aliases/pg-native.js'),
-    'pgpass$': path.join(__dirname, 'aliases/pgpass.js'),
+    'pg-native': path.join(__dirname, 'alias/pg-native.js'),
+    'pgpass$': path.join(__dirname, 'alias/pgpass.js'),
   },
+  plugins: [
+    new webpack.IgnorePlugin(/^pg-native$/)
+  ]
+
 }
