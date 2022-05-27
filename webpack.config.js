08e3f4b2bc34bdc9e11357966c12d92cd7ed758d
Gál Péter <galpe23@gmail.com>
2022-05-27 02:36:58 +0200

    +10?

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,9 @@ module.exports = {
   optimization: {minimize: false},
   devtool: "source-map",
   target: "node",
+  plugins: [
+    new webpack.IgnorePlugin(/^pg-native$/),
+    // Or, for WebPack 4+:
+    new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })
+  ],
 }
