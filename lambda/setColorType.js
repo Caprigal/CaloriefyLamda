b251d50a6ef5b5e5b4dedecaa22b79604c3fbce4
Gál Péter <galpe23@gmail.com>
2022-11-28 19:28:15 +0100

    home updates

---
+++ lambda/setColorType.js
@@ -0,0 +1,45 @@
+const pg = require('pg')
+require('dotenv').config()
+const headers = {
+  'access-control-allow-methods': '*',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  'Access-Control-Max-Age': '2592000',
+  'Access-Control-Allow-Credentials': 'true',
+}
+
+exports.handler = async (event, context) => {
+  const client = new pg.Client(process.env.PSQL_CONN_STRING)
+
+  /* Handle httpMethod variations and errors */
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: headers,
+    }
+  }
+
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, headers: headers, body: 'Method Not Allowed' }
+  }
+
+  try {
+    client.connect()
+
+    const response = await client.query(`UPDATE home SET color_type='${event.queryStringParameters.ct}' WHERE id='c01217ba-d6e5-4d88-93df-8bb9ff64c03e'`)
+
+    client.end()
+    return {
+      statusCode: 200,
+      headers: headers,
+      body: JSON.stringify(response.rows),
+    }
+  } catch (err) {
+    client.end()
+    return {
+      statusCode: 400,
+      headers: headers,
+      body: JSON.stringify({ stack: err.stack, message: err.message }),
+    }
+  }
+}
